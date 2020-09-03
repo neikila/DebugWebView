8b45cb60a17abfcaa74ff94c5910dff322a3fb07
neikila <neikila@gmail.com>
2020-09-03 13:19:05 +0300

    init

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.webviewapp
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.settings.javaScriptEnabled = true
+        WebView.setWebContentsDebuggingEnabled(true)
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return false
+            }
+        }
+        myWebView.loadUrl("https://eda-dev.vmb.co/public_web_api/registration_utils/get_link.json?protocol=https")
+    }
+}
